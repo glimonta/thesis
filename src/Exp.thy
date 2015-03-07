e4dd5f73a20ade31c9cf3ab8d9165f80f8e6a1cb
Gabriela Limonta <glimonta@gmail.com>
2015-03-07 14:35:31 +0100

    eval function ready.
    So far I ignored the exp Free, I think it should be a statement instead of
    an expression.
    
    Next step lexp and eval_l.

--- src/Exp.thy
+++ src/Exp.thy
@@ -6,8 +6,8 @@ begin
 
 (* Variable names are strings *)
 type_synonym vname = string
-(* Addresses are integers *)
-type_synonym addr = int
+(* Addresses are a pair of integers (block_id, offset) *)
+type_synonym addr = "int \<times> int"
 (* A value can be either an integer or an address *)
 datatype val = I int | A addr
 (*
@@ -22,6 +22,12 @@ type_synonym loc = "vname \<Rightarrow> val"
 type_synonym mem = "val list list"
 type_synonym state = "loc \<times> mem"
 
+(* lists will be accessed by 1 in the first position because A (0,0) is null addr *)
+fun inth :: "'a list \<Rightarrow> int \<Rightarrow> 'a" (infixl "!!" 100) where
+"(x # xs) !! i = (if i = 1 then x else xs !! (i - 1))"
+
+abbreviation "list_size xs \<equiv> int(length xs)"
+
 datatype exp = Const val
              | Null           (* This is to separate addresses from values *)
              | V     vname
@@ -35,8 +41,10 @@ datatype exp = Const val
              | Ref   exp     (* & *)
              | Index exp exp (* e[e] *)
 
+(*
 datatype lexp = Deref exp
               | Index exp exp
+*)
 
 fun plus_val :: "val \<Rightarrow> val \<Rightarrow> val option" where
   "plus_val (I i\<^sub>1) (I i\<^sub>2) = Some (I (i\<^sub>1 + i\<^sub>2))"
@@ -47,13 +55,42 @@ fun less_val :: "val \<Rightarrow> val \<Rightarrow> val option" where
 | "less_val a\<^sub>1 a\<^sub>2 = None"
 
 
-fun neg_val :: "val \<Rightarrow> val option" where
-  "neg_val (I i) = (if i = 0 then Some (I 1) else Some (I 0))"
-| "neg_val a = None"
+fun not_val :: "val \<Rightarrow> val option" where
+  "not_val (I i) = (if i = 0 then Some (I 1) else Some (I 0))"
+| "not_val a = None"
 
 fun and_val :: "val \<Rightarrow> val \<Rightarrow> val option" where
   "and_val (I i\<^sub>1) (I i\<^sub>2) = (if i\<^sub>1 = 0 then Some (I 0) else (if i\<^sub>2 = 0 then Some (I 0) else Some (I 1)))"
 | "and_val a\<^sub>1 a\<^sub>2 = None"
+
+(* Returns a new block of memory of size v, initialized with 0s *)
+fun init_block :: "nat \<Rightarrow> val list" where
+  "init_block 0 = []"
+| "init_block (Suc i) = (I 0) # init_block i"
+
+(* Allocates a new block in the memory *)
+(* 
+  This is using conversions between int and nat I don't know what happens if the number in 
+  (I i) is neg
+*)
+fun new_block :: "val \<Rightarrow> mem \<Rightarrow> mem option" where
+  "new_block (I i) \<mu> = (if i < list_size \<mu> then None else Some (\<mu> @ [init_block (nat i)]))"
+| "new_block (A a) _ = None"
+
+value "new_block (I 2) [[]]"
+
+(* In indexes 0 nothing should be stored *)
+fun get_mem :: "val \<Rightarrow> mem \<Rightarrow> val option" where
+  "get_mem (I i) _ = None"
+| "get_mem (A (i,j)) \<mu> = (if (i=0 \<or> j=0) then None else Some ((\<mu> !! i) !! j))"
+
+value "get_mem (A (0,0)) [[(I 1), (I 2)], [(I 3), (I 4), (I 5)]]"
+value "get_mem (A (1,2)) [[(I 1), (I 2)], [(I 3), (I 4), (I 5)]]"
+value "get_mem (A (2,3)) [[(I 1), (I 2)], [(I 3), (I 4), (I 5)]]"
+
+fun index_mem :: "val \<Rightarrow> val \<Rightarrow> mem \<Rightarrow> val option" where
+  "index_mem (A (x,y)) (I i) \<mu> = get_mem (A (x, (y + i))) \<mu>"
+| "index_mem _ _ _ = None"
 
 (*
   The return here is a pair (val \<times> state) option. The state is necessary because malloc and free
@@ -62,7 +99,7 @@ fun and_val :: "val \<Rightarrow> val \<Rightarrow> val option" where
 *)
 fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" where
   "eval (Const c) s = Some (c, s)"
-| "eval Null s = Some (A 0, s)"
+| "eval Null s = Some (A (0,0), s)"
 | "eval (V x) (\<sigma>, \<mu>) = Some (\<sigma> x, (\<sigma>, \<mu>))"
 | "eval (Plus i\<^sub>1 i\<^sub>2) s =  (case (eval i\<^sub>1 s) of
                             None \<Rightarrow> None |
@@ -80,7 +117,7 @@ fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" 
                                                                   Some v \<Rightarrow> Some (v, s''))))"
 | "eval (Not b) s = (case (eval b s) of 
                        None \<Rightarrow> None |
-                       Some (b', s) \<Rightarrow> (case (neg_val b') of
+                       Some (b', s) \<Rightarrow> (case (not_val b') of
                                           None \<Rightarrow> None |
                                           Some b'' \<Rightarrow> Some (b'', s)))"
 | "eval (And b\<^sub>1 b\<^sub>2) s = (case (eval b\<^sub>1 s) of
@@ -90,11 +127,28 @@ fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" 
                                               Some (v\<^sub>2, s'') \<Rightarrow> (case (and_val v\<^sub>1 v\<^sub>2) of
                                                                   None \<Rightarrow> None |
                                                                   Some v \<Rightarrow> Some (v, s''))))"
-(*
 | "eval (New e) s = (case (eval e s) of
                        None \<Rightarrow> None |
-                       Some (v, (\<sigma>, \<mu>)) \<Rightarrow> (,))"
-*)
+                       Some (v, (\<sigma>, \<mu>)) \<Rightarrow> (case (new_block v \<mu>) of
+                                              None \<Rightarrow> None |
+                                              Some \<mu>' \<Rightarrow> Some ((I (list_size \<mu>')), (\<sigma>, \<mu>'))))"
+| "eval (Deref e) s = (case (eval e s) of
+                         None \<Rightarrow> None |
+                         Some (v, (\<sigma>, \<mu>)) \<Rightarrow> (case (get_mem v \<mu>) of
+                                                None \<Rightarrow> None |
+                                                Some v' \<Rightarrow> Some (v', (\<sigma>,\<mu>))))"
+| "eval (Ref e) s = (case (eval e s) of
+                       None \<Rightarrow> None |
+                       Some (v, s') \<Rightarrow> (case v of
+                                          (I _) \<Rightarrow> None |
+                                          (A _) \<Rightarrow> Some (v,s')))"
+| "eval (Index e\<^sub>1 e\<^sub>2) s = (case (eval e\<^sub>1 s) of
+                            None \<Rightarrow> None |
+                            Some (v\<^sub>1, s') \<Rightarrow> (case (eval e\<^sub>2 s') of
+                                               None \<Rightarrow> None |
+                                               Some (v\<^sub>2, (\<sigma>, \<mu>)) \<Rightarrow> (case (index_mem v\<^sub>1 v\<^sub>2 \<mu>) of
+                                                                      None \<Rightarrow> None |
+                                                                      Some v \<Rightarrow> Some(v, (\<sigma>,\<mu>)))))"
 
 (*
 text_raw{*\snip{AExpaexpdef}{2}{1}{% *}
