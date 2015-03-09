102f7808c75cb2b17573a1b6d0f3f776cab87c03
Gabriela Limonta <glimonta@gmail.com>
2015-03-09 13:43:27 +0100

    eval and eval_l ready.
    Using different names for exp and lexp constructors was ok.
    Datatype and functions are defined to be mutually recursive.

--- src/Exp.thy
+++ src/Exp.thy
@@ -35,13 +35,12 @@ datatype exp = Const val
              | Not   exp
              | And   exp exp
              | New   exp
-             | Free  exp
-             | Deref exp     (* * *)
-             | Ref   exp     (* & *)
+             | Deref exp    (* * *)
+             | Ref   lexp    (* & *)
              | Index exp exp (* e[e] *)
 
-datatype lexp = Derefl exp
-              | Indexl exp exp
+and lexp = Derefl exp
+         | Indexl exp exp
 
 fun plus_val :: "val \<Rightarrow> val \<Rightarrow> val option" where
   "plus_val (I i\<^sub>1) (I i\<^sub>2) = Some (I (i\<^sub>1 + i\<^sub>2))"
@@ -95,7 +94,8 @@ fun index_mem :: "val \<Rightarrow> val \<Rightarrow> mem \<Rightarrow> val opti
   are expressions and those modify the state. We treate this as an option because None would be
   a special error state in which the evaluation got stuck i.e. sum of two pointers.
 *)
-fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" where
+fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option"
+and eval_l :: "lexp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" where
   "eval (Const c) s = Some (c, s)"
 | "eval Null s = Some (A (0,0), s)"
 | "eval (V x) (\<sigma>, \<mu>) = Some (\<sigma> x, (\<sigma>, \<mu>))"
@@ -135,7 +135,7 @@ fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" 
                          Some (v, (\<sigma>, \<mu>)) \<Rightarrow> (case (get_mem v \<mu>) of
                                                 None \<Rightarrow> None |
                                                 Some v' \<Rightarrow> Some (v', (\<sigma>,\<mu>))))"
-| "eval (Ref e) s = (case (eval e s) of
+| "eval (Ref e) s = (case (eval_l e s) of
                        None \<Rightarrow> None |
                        Some (v, s') \<Rightarrow> (case v of
                                           (I _) \<Rightarrow> None |
@@ -147,9 +147,7 @@ fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" 
                                                Some (v\<^sub>2, (\<sigma>, \<mu>)) \<Rightarrow> (case (index_mem v\<^sub>1 v\<^sub>2 \<mu>) of
                                                                       None \<Rightarrow> None |
                                                                       Some v \<Rightarrow> Some(v, (\<sigma>,\<mu>)))))"
-
-fun eval_l :: "lexp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" where
-  "eval_l (Derefl e) s = (case (eval e s) of
+| "eval_l (Derefl e) s = (case (eval e s) of
                             None \<Rightarrow> None |
                             Some (v, s') \<Rightarrow> (case v of
                                                (I _) \<Rightarrow> None |
@@ -161,110 +159,6 @@ fun eval_l :: "lexp \<Rightarrow> state \<Rightarrow> (val \<times> state) optio
                                                   Some (v\<^sub>2, s'') \<Rightarrow> (case (plus_val v\<^sub>1 v\<^sub>2) of
                                                                       None \<Rightarrow> None |
                                                                       Some v \<Rightarrow> Some(v, s''))))"
-
-(*
-text_raw{*\snip{AExpaexpdef}{2}{1}{% *}
-datatype aexp = N int | V vname | Plus aexp aexp
-text_raw{*}%endsnip*}
-
-text_raw{*\snip{AExpavaldef}{1}{2}{% *}
-fun aval :: "aexp \<Rightarrow> state \<Rightarrow> val" where
-"aval (N n) s = n" |
-"aval (V x) s = s x" |
-"aval (Plus a\<^sub>1 a\<^sub>2) s = aval a\<^sub>1 s + aval a\<^sub>2 s"
-text_raw{*}%endsnip*}
-
-
-value "aval (Plus (V ''x'') (N 5)) (\<lambda>x. if x = ''x'' then 7 else 0)"
-
-text {* The same state more concisely: *}
-value "aval (Plus (V ''x'') (N 5)) ((\<lambda>x. 0) (''x'':= 7))"
-
-text {* A little syntax magic to write larger states compactly: *}
-
-definition null_state ("<>") where
-  "null_state \<equiv> \<lambda>x. 0"
-syntax 
-  "_State" :: "updbinds => 'a" ("<_>")
-translations
-  "_State ms" == "_Update <> ms"
-  "_State (_updbinds b bs)" <= "_Update (_State b) bs"
-
-text {* \noindent
-  We can now write a series of updates to the function @{text "\<lambda>x. 0"} compactly:
-*}
-lemma "<a := 1, b := 2> = (<> (a := 1)) (b := (2::int))"
-  by (rule refl)
-
-value "aval (Plus (V ''x'') (N 5)) <''x'' := 7>"
-
-
-text {* In  the @{term[source] "<a := b>"} syntax, variables that are not mentioned are 0 by default:
-*}
-value "aval (Plus (V ''x'') (N 5)) <''y'' := 7>"
-
-text{* Note that this @{text"<\<dots>>"} syntax works for any function space
-@{text"\<tau>\<^sub>1 \<Rightarrow> \<tau>\<^sub>2"} where @{text "\<tau>\<^sub>2"} has a @{text 0}. *}
-
-
-subsection "Constant Folding"
-
-text{* Evaluate constant subsexpressions: *}
-
-text_raw{*\snip{AExpasimpconstdef}{0}{2}{% *}
-fun asimp_const :: "aexp \<Rightarrow> aexp" where
-"asimp_const (N n) = N n" |
-"asimp_const (V x) = V x" |
-"asimp_const (Plus a\<^sub>1 a\<^sub>2) =
-  (case (asimp_const a\<^sub>1, asimp_const a\<^sub>2) of
-    (N n\<^sub>1, N n\<^sub>2) \<Rightarrow> N(n\<^sub>1+n\<^sub>2) |
-    (b\<^sub>1,b\<^sub>2) \<Rightarrow> Plus b\<^sub>1 b\<^sub>2)"
-text_raw{*}%endsnip*}
-
-theorem aval_asimp_const:
-  "aval (asimp_const a) s = aval a s"
-apply(induction a)
-apply (auto split: aexp.split)
-done
-
-text{* Now we also eliminate all occurrences 0 in additions. The standard
-method: optimized versions of the constructors: *}
-
-text_raw{*\snip{AExpplusdef}{0}{2}{% *}
-fun plus :: "aexp \<Rightarrow> aexp \<Rightarrow> aexp" where
-"plus (N i\<^sub>1) (N i\<^sub>2) = N(i\<^sub>1+i\<^sub>2)" |
-"plus (N i) a = (if i=0 then a else Plus (N i) a)" |
-"plus a (N i) = (if i=0 then a else Plus a (N i))" |
-"plus a\<^sub>1 a\<^sub>2 = Plus a\<^sub>1 a\<^sub>2"
-text_raw{*}%endsnip*}
-
-lemma aval_plus[simp]:
-  "aval (plus a1 a2) s = aval a1 s + aval a2 s"
-apply(induction a1 a2 rule: plus.induct)
-apply simp_all (* just for a change from auto *)
-done
-
-text_raw{*\snip{AExpasimpdef}{2}{0}{% *}
-fun asimp :: "aexp \<Rightarrow> aexp" where
-"asimp (N n) = N n" |
-"asimp (V x) = V x" |
-"asimp (Plus a\<^sub>1 a\<^sub>2) = plus (asimp a\<^sub>1) (asimp a\<^sub>2)"
-text_raw{*}%endsnip*}
-
-text{* Note that in @{const asimp_const} the optimized constructor was
-inlined. Making it a separate function @{const plus} improves modularity of
-the code and the proofs. *}
-
-value "asimp (Plus (Plus (N 0) (N 0)) (Plus (V ''x'') (N 0)))"
-
-theorem aval_asimp[simp]:
-  "aval (asimp a) s = aval a s"
-apply(induction a)
-apply simp_all
-done
-
-end
-*)
 
 
 
