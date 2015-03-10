14000e72437619146bffe16312bf1b5615e1440b
Gabriela Limonta <glimonta@gmail.com>
2015-03-10 12:40:38 +0100

    Changed the expression as discussed today.
    * init_block was changed for the replicate function.
    * new_block now returns the address of the newly allocated block along with the new state of the memory.
    * eval and eval_l were changed to use the nice do-notation, only cases I didn't change were eval Ref and eval_l Derefl.

--- src/Exp.thy
+++ src/Exp.thy
@@ -7,7 +7,7 @@ type_synonym vname = string
 (* Addresses are a pair of integers (block_id, offset) *)
 type_synonym addr = "int \<times> int"
 (* A value can be either an integer or an address *)
-datatype val = I int | A addr
+datatype val = NullVal | I int | A addr
 (*
   A state is a pair in which the first element represents the content of the local variables
   and the second element represents the content of the blocked memory.
@@ -20,9 +20,8 @@ type_synonym loc = "vname \<Rightarrow> val"
 type_synonym mem = "val list list"
 type_synonym state = "loc \<times> mem"
 
-(* lists will be accessed by 1 in the first position because A (0,0) is null addr *)
 fun inth :: "'a list \<Rightarrow> int \<Rightarrow> 'a" (infixl "!!" 100) where
-"(x # xs) !! i = (if i = 1 then x else xs !! (i - 1))"
+"(x # xs) !! i = (if i = 0 then x else xs !! (i - 1))"
 
 abbreviation "list_size xs \<equiv> int(length xs)"
 
@@ -60,26 +59,21 @@ fun and_val :: "val \<Rightarrow> val \<Rightarrow> val option" where
   "and_val (I i\<^sub>1) (I i\<^sub>2) = (if i\<^sub>1 = 0 then Some (I 0) else (if i\<^sub>2 = 0 then Some (I 0) else Some (I 1)))"
 | "and_val a\<^sub>1 a\<^sub>2 = None"
 
-(* Returns a new block of memory of size v, initialized with 0s *)
-fun init_block :: "nat \<Rightarrow> val list" where
-  "init_block 0 = []"
-| "init_block (Suc i) = (I 0) # init_block i"
- term replicate
 (* Allocates a new block in the memory *)
-(* 
+(* n
   This is using conversions between int and nat I don't know what happens if the number in 
   (I i) is neg
 *)
-fun new_block :: "val \<Rightarrow> mem \<Rightarrow> mem option" where
-  "new_block (I i) \<mu> = Some (\<mu> @ [init_block (nat i)])"
-| "new_block (A a) _ = None"
+fun new_block :: "val \<Rightarrow> mem \<Rightarrow> (val \<times> mem) option" where
+  "new_block (I i) \<mu> = Some ((A (list_size \<mu>, 0)), \<mu> @ [replicate (nat i) (I 0)])"
+| "new_block _ _ = None"
 
 value "new_block (I 2) [[]]"
 
 (* In indexes 0 nothing should be stored *)
 fun get_mem :: "val \<Rightarrow> mem \<Rightarrow> val option" where
-  "get_mem (I i) _ = None"
-| "get_mem (A (i,j)) \<mu> = (if (i=0 \<or> j=0) then None else Some ((\<mu> !! i) !! j))"
+  "get_mem (A (i,j)) \<mu> = (if (i=0 \<or> j=0) then None else Some ((\<mu> !! i) !! j))"  
+| "get_mem _ _ = None"
 
 value "get_mem (A (0,0)) [[(I 1), (I 2)], [(I 3), (I 4), (I 5)]]"
 value "get_mem (A (1,2)) [[(I 1), (I 2)], [(I 3), (I 4), (I 5)]]"
@@ -91,84 +85,68 @@ fun index_mem :: "val \<Rightarrow> val \<Rightarrow> mem \<Rightarrow> val opti
 
 (*
   The return here is a pair (val \<times> state) option. The state is necessary because malloc and free
-  are expressions and those modify the state. We treate this as an option because None would be
+  are expressions and those modify the state. We treat this as an option because None would be
   a special error state in which the evaluation got stuck i.e. sum of two pointers.
 *)
 fun eval :: "exp \<Rightarrow> state \<Rightarrow> (val \<times> state) option"
 and eval_l :: "lexp \<Rightarrow> state \<Rightarrow> (val \<times> state) option" where
   "eval (Const c) s = Some (c, s)"
-| "eval Null s = Some (A (0,0), s)"
+| "eval Null s = Some (NullVal, s)"
 | "eval (V x) (\<sigma>, \<mu>) = Some (\<sigma> x, (\<sigma>, \<mu>))"
-| "eval (Plus i\<^sub>1 i\<^sub>2) s =  (case (eval i\<^sub>1 s) of
-                            None \<Rightarrow> None |
-                            Some (v\<^sub>1, s') \<Rightarrow> (case (eval i\<^sub>2 s') of
-                                               None \<Rightarrow> None |
-                                               Some (v\<^sub>2, s'') \<Rightarrow> (case (plus_val v\<^sub>1 v\<^sub>2) of
-                                                                   None \<Rightarrow> None |
-                                                                   Some v \<Rightarrow> Some (v, s''))))"
-| "eval (Less i\<^sub>1 i\<^sub>2) s = (case (eval i\<^sub>1 s) of
-                           None \<Rightarrow> None |
-                           Some (v\<^sub>1, s') \<Rightarrow> (case (eval i\<^sub>2 s') of
-                                              None \<Rightarrow> None |
-                                              Some (v\<^sub>2, s'') \<Rightarrow> (case (less_val v\<^sub>1 v\<^sub>2) of
-                                                                  None \<Rightarrow> None |
-                                                                  Some v \<Rightarrow> Some (v, s''))))"
-| "eval (Not b) s = (case (eval b s) of 
-                       None \<Rightarrow> None |
-                       Some (b', s) \<Rightarrow> (case (not_val b') of
-                                          None \<Rightarrow> None |
-                                          Some b'' \<Rightarrow> Some (b'', s)))"
-| "eval (And b\<^sub>1 b\<^sub>2) s = (case (eval b\<^sub>1 s) of
-                           None \<Rightarrow> None |
-                           Some (v\<^sub>1, s') \<Rightarrow> (case (eval b\<^sub>2 s') of
-                                              None \<Rightarrow> None |
-                                              Some (v\<^sub>2, s'') \<Rightarrow> (case (and_val v\<^sub>1 v\<^sub>2) of
-                                                                  None \<Rightarrow> None |
-                                                                  Some v \<Rightarrow> Some (v, s''))))"
-| "eval (New e) s = (case (eval e s) of
-                       None \<Rightarrow> None |
-                       Some (v, (\<sigma>, \<mu>)) \<Rightarrow> (case (new_block v \<mu>) of
-                                              None \<Rightarrow> None |
-                                              Some \<mu>' \<Rightarrow> Some ((I (list_size \<mu>')), (\<sigma>, \<mu>'))))"
-| "eval (Deref e) s = (case (eval e s) of
-                         None \<Rightarrow> None |
-                         Some (v, (\<sigma>, \<mu>)) \<Rightarrow> (case (get_mem v \<mu>) of
-                                                None \<Rightarrow> None |
-                                                Some v' \<Rightarrow> Some (v', (\<sigma>,\<mu>))))"
+| "eval (Plus i\<^sub>1 i\<^sub>2) s = do {
+  (v\<^sub>1, s) \<leftarrow> eval i\<^sub>1 s;
+  (v\<^sub>2, s) \<leftarrow> eval i\<^sub>2 s;
+  v \<leftarrow> plus_val v\<^sub>1 v\<^sub>2;
+  Some (v, s)
+}"
+| "eval (Less i\<^sub>1 i\<^sub>2) s = do {
+  (v\<^sub>1, s) \<leftarrow> eval i\<^sub>1 s;
+  (v\<^sub>2, s) \<leftarrow> eval i\<^sub>2 s;
+  v \<leftarrow> less_val v\<^sub>1 v\<^sub>2;
+  Some (v, s)
+}"
+| "eval (Not b) s = do {
+  (v, s) \<leftarrow> eval b s;
+  v \<leftarrow> not_val v;
+  Some (v, s)
+}"
+| "eval (And b\<^sub>1 b\<^sub>2) s = do {
+  (v\<^sub>1, s) \<leftarrow> eval b\<^sub>1 s;
+  (v\<^sub>2, s) \<leftarrow> eval b\<^sub>2 s;
+  v \<leftarrow> and_val v\<^sub>1 v\<^sub>2;
+  Some (v, s)
+}"
+| "eval (New e) s = do {
+  (v, (\<sigma>, \<mu>)) \<leftarrow> eval e s;
+  (v, \<mu>) \<leftarrow> new_block v \<mu>;
+  Some (v, (\<sigma>, \<mu>))
+}"
+| "eval (Deref e) s = do {
+  (v, (\<sigma>, \<mu>)) \<leftarrow> eval e s;
+  v \<leftarrow> get_mem v \<mu>;
+  Some (v, (\<sigma>, \<mu>))
+}"
 | "eval (Ref e) s = (case (eval_l e s) of
                        None \<Rightarrow> None |
                        Some (v, s') \<Rightarrow> (case v of
                                           (I _) \<Rightarrow> None |
                                           (A _) \<Rightarrow> Some (v,s')))"
-| "eval (Index e\<^sub>1 e\<^sub>2) s = (case (eval e\<^sub>1 s) of
-                            None \<Rightarrow> None |
-                            Some (v\<^sub>1, s') \<Rightarrow> (case (eval e\<^sub>2 s') of
-                                               None \<Rightarrow> None |
-                                               Some (v\<^sub>2, (\<sigma>, \<mu>)) \<Rightarrow> (case (index_mem v\<^sub>1 v\<^sub>2 \<mu>) of
-                                                                      None \<Rightarrow> None |
-                                                                      Some v \<Rightarrow> Some(v, (\<sigma>,\<mu>)))))"
+| "eval (Index e\<^sub>1 e\<^sub>2) s = do {
+  (v\<^sub>1, s) \<leftarrow> eval e\<^sub>1 s;
+  (v\<^sub>2, (\<sigma>, \<mu>)) \<leftarrow> eval e\<^sub>2 s;
+  v \<leftarrow> index_mem v\<^sub>1 v\<^sub>2 \<mu>;
+  Some (v, (\<sigma>, \<mu>))
+}"
 | "eval_l (Derefl e) s = (case (eval e s) of
                             None \<Rightarrow> None |
                             Some (v, s') \<Rightarrow> (case v of
                                                (I _) \<Rightarrow> None |
                                                (A _) \<Rightarrow> Some (v, s')))"
-| "eval_l (Indexl e\<^sub>1 e\<^sub>2) s = (case (eval e\<^sub>1 s) of
-                               None \<Rightarrow> None |
-                               Some (v\<^sub>1, s') \<Rightarrow> (case (eval e\<^sub>2 s') of
-                                                  None \<Rightarrow> None |
-                                                  Some (v\<^sub>2, s'') \<Rightarrow> (case (plus_val v\<^sub>1 v\<^sub>2) of
-                                                                      None \<Rightarrow> None |
-                                                                      Some v \<Rightarrow> Some(v, s''))))"
-
-lemma "eval_l (Indexl e1 e2) s = do {
-  (v1,s) \<leftarrow> eval e1 s;
-  (v2,s) \<leftarrow> eval e2 s;
-  r \<leftarrow> plus_val v1 v2;
-  Some (r,s)
+| "eval_l (Indexl e\<^sub>1 e\<^sub>2) s = do {
+  (v\<^sub>1, s) \<leftarrow> eval e\<^sub>1 s;
+  (v\<^sub>2, s) \<leftarrow> eval e\<^sub>2 s;
+  v \<leftarrow> plus_val v\<^sub>1 v\<^sub>2;
+  Some (v, s)
 }"
-  by (simp split: option.splits)
-
-
-
 
 end
