4fd0318549969aff2958708b719bcd7975f29060
Gabriela Limonta <glimonta@gmail.com>
2015-03-10 14:48:38 +0100

    Added syntax for Free

--- src/Com.thy
+++ src/Com.thy
@@ -4,10 +4,11 @@ begin
 
 datatype
   com = SKIP 
-      | Assign lexp exp       ("_ ::= _" [1000, 61] 61)
-      | Seq    com  com         ("_;;/ _"  [60, 61] 60)
-      | If     exp com com     ("(IF _/ THEN _/ ELSE _)"  [0, 0, 61] 61)
-      | While  exp com         ("(WHILE _/ DO _)"  [0, 61] 61)
-      | Free lexp
+      | Assignl lexp exp       ("_ ::== _" [1000, 61] 61)
+      | Assign  vname exp      ("_ ::= _" [1000, 61] 61)
+      | Seq     com  com       ("_;;/ _"  [60, 61] 60)
+      | If      exp com com    ("(IF _/ THEN _/ ELSE _)"  [0, 0, 61] 61)
+      | While   exp com        ("(WHILE _/ DO _)"  [0, 61] 61)
+      | Free    lexp           ("FREE _" [0])
 
 end
