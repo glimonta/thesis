f22f87a44f1ff6f3e9d0a5eadcbc427d263503fb
Gabriela Limonta <glimonta@gmail.com>
2015-05-07 14:26:30 +0200

    Several example programs are added.
    *  bubblesort
    *  min of an array
    *  occurs of an element in an array (bool)
    *  number of occurrences of an element in an array
    *  selection sort
    *  single linked list creation

--- src/Test.thy
+++ src/Test.thy
@@ -104,7 +104,7 @@ definition "bubblesort_test \<equiv>
 "
 
 value
-  "case interp (bubblesort_test, initial_state) of Some (_,(l,m)) \<Rightarrow> (l bb, m)"
+  "case interp (bubblesort_test, initial_state) of Some (_,(l,m)) \<Rightarrow> (l aa, m)"
 
 ML {*
 
@@ -118,5 +118,146 @@ val r = l [#"a"]
 val r2 = hd m
 *}
 
+definition "min_test \<equiv>
+  aa ::= New (Const (I 10));;
+  (Indexl (V aa) (Const (I 0))) ::== (Const (I 44));;
+  (Indexl (V aa) (Const (I 1))) ::== (Const (I 98));;
+  (Indexl (V aa) (Const (I 2))) ::== (Const (I 60));;
+  (Indexl (V aa) (Const (I 3))) ::== (Const (I 26));;
+  (Indexl (V aa) (Const (I 4))) ::== (Const (I 54));;
+  (Indexl (V aa) (Const (I 5))) ::== (Const (I 1));;
+  (Indexl (V aa) (Const (I 6))) ::== (Const (I 92));;
+  (Indexl (V aa) (Const (I 7))) ::== (Const (I 84));;
+  (Indexl (V aa) (Const (I 8))) ::== (Const (I 38));;
+  (Indexl (V aa) (Const (I 9))) ::== (Const (I 80));;
+
+  nn ::= (Const (I 10));; (* Length of array *)
+  mm ::= (Index (V aa) (Const (I 0)));; (* Min *)
+
+
+  FOR ii FROM (Const (I 0)) TO (Plus (V nn) (Const (I -1))) DO
+    (IF (Less (Index (V aa) (V ii)) (V mm))
+     THEN mm ::= (Index (V aa) (V ii))
+     ELSE SKIP)
+"
+
+value
+  "case interp (min_test, initial_state) of Some (_,(l,_)) \<Rightarrow> (l mm)"
+
+definition "occurs_test x \<equiv>
+  aa ::= New (Const (I 10));;
+  (Indexl (V aa) (Const (I 0))) ::== (Const (I 44));;
+  (Indexl (V aa) (Const (I 1))) ::== (Const (I 98));;
+  (Indexl (V aa) (Const (I 2))) ::== (Const (I 60));;
+  (Indexl (V aa) (Const (I 3))) ::== (Const (I 26));;
+  (Indexl (V aa) (Const (I 4))) ::== (Const (I 54));;
+  (Indexl (V aa) (Const (I 5))) ::== (Const (I 1));;
+  (Indexl (V aa) (Const (I 6))) ::== (Const (I 92));;
+  (Indexl (V aa) (Const (I 7))) ::== (Const (I 84));;
+  (Indexl (V aa) (Const (I 8))) ::== (Const (I 38));;
+  (Indexl (V aa) (Const (I 9))) ::== (Const (I 80));;
+
+  nn ::= (Const (I 10));; (* Length of array *)
+  xx ::= (Const (I x));;
+  bb ::= (Const (I 0));; (* Default: False (value does not occur) *)
+
+
+  FOR ii FROM (Const (I 0)) TO (Plus (V nn) (Const (I -1))) DO
+    (IF (Eq (Index (V aa) (V ii)) (V xx))
+     THEN bb ::= (Const (I 1))
+     ELSE SKIP)
+"
+
+(* The element doesn't exist *)
+value
+  "case interp (occurs_test 5, initial_state) of Some (_,(l,_)) \<Rightarrow> (l bb)"
+
+(* The element exists *)
+value
+  "case interp (occurs_test 84, initial_state) of Some (_,(l,_)) \<Rightarrow> (l bb)"
+
+definition "count_test x \<equiv>
+  bb ::= (Const (I 0));;
+  aa ::= New (Const (I 10));;
+  (Indexl (V aa) (Const (I 0))) ::== (Const (I 44));;
+  (Indexl (V aa) (Const (I 1))) ::== (Const (I 98));;
+  (Indexl (V aa) (Const (I 2))) ::== (Const (I 44));;
+  (Indexl (V aa) (Const (I 3))) ::== (Const (I 44));;
+  (Indexl (V aa) (Const (I 4))) ::== (Const (I 54));;
+  (Indexl (V aa) (Const (I 5))) ::== (Const (I 1));;
+  (Indexl (V aa) (Const (I 6))) ::== (Const (I 92));;
+  (Indexl (V aa) (Const (I 7))) ::== (Const (I 84));;
+  (Indexl (V aa) (Const (I 8))) ::== (Const (I 44));;
+  (Indexl (V aa) (Const (I 9))) ::== (Const (I 44));;
+
+  nn ::= (Const (I 10));; (* Length of array *)
+  xx ::= (Const (I x));;
+  cc ::= (Const (I 0));; (* Counter of occurences *)
+  dd ::= (Index (V aa) (Const (I 9)));;
+
+  FOR ii FROM (Const (I 0)) TO (V nn) DO
+    (IF (Eq (Index (V aa) (V ii)) (V xx))
+     THEN cc ::= (Plus (V cc) (Const (I 1)))
+     ELSE SKIP)
+"
+
+(* The element doesn't exist *)
+value
+  "case interp (count_test 5, initial_state) of Some (_,(l,_)) \<Rightarrow> (l cc)"
+
+(* The element exists once *)
+value
+  "case interp (count_test 84, initial_state) of Some (_,(l,_)) \<Rightarrow> (l cc)"
+
+(* The element exists more than once *)
+value
+  "case interp (count_test 44, initial_state) of Some (_,(l,_)) \<Rightarrow> (l cc)"
+
+definition "selectionsort_test \<equiv>
+  aa ::= New (Const (I 10));;
+  (Indexl (V aa) (Const (I 0))) ::== (Const (I 44));;
+  (Indexl (V aa) (Const (I 1))) ::== (Const (I 98));;
+  (Indexl (V aa) (Const (I 2))) ::== (Const (I 60));;
+  (Indexl (V aa) (Const (I 3))) ::== (Const (I 26));;
+  (Indexl (V aa) (Const (I 4))) ::== (Const (I 54));;
+  (Indexl (V aa) (Const (I 5))) ::== (Const (I 1));;
+  (Indexl (V aa) (Const (I 6))) ::== (Const (I 92));;
+  (Indexl (V aa) (Const (I 7))) ::== (Const (I 84));;
+  (Indexl (V aa) (Const (I 8))) ::== (Const (I 38));;
+  (Indexl (V aa) (Const (I 9))) ::== (Const (I 80));;
+
+  nn ::= (Const (I 10));;
+
+
+  FOR ii FROM (Const (I 0)) TO (Plus (V nn) (Const (I -1))) DO
+    (mm ::= (Index (V aa) (V ii));; (* Min *)
+    tt ::= (V ii);; (* Min index *)
+    (FOR jj FROM (Plus (V ii) (Const (I 1))) TO (V nn) DO
+      (IF (Less (Index (V aa) (V jj)) (V mm))
+      THEN 
+        mm ::= (Index (V aa) (V jj));;
+        tt ::= (V jj)
+      ELSE SKIP));;
+    (Indexl (V aa) (V tt)) ::== (Index (V aa) (V ii));;
+    (Indexl (V aa) (V ii)) ::== (V mm))
+"
+
+value
+  "case interp (selectionsort_test, initial_state) of Some (_,(l,m)) \<Rightarrow> (l aa, m)"
+
+(* Creates a new list of size n initialized in zeros *)
+definition "newlist_test n \<equiv>
+  ll ::= New (Const (I 1));;
+  nn ::= (Const (I n));;
+
+  FOR ii FROM (Const (I 0)) TO (V nn) DO
+  (xx ::= New (Const (I 2));;
+  (Indexl (V xx) (Const (I 0))) ::== (Const (I 0));;
+  (Indexl (V xx) (Const (I 1))) ::== (Deref (V ll));;
+  (Derefl (V ll)) ::== (V xx))
+"
+
+value
+  "case interp (newlist_test 5, initial_state) of Some (_,(l,m)) \<Rightarrow> (l ll, m)"
 
 end
