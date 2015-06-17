4d1e7ad0999a98828645e231515a4778496c106d
Gabriela Limonta <glimonta@gmail.com>
2015-06-17 11:11:44 +0200

    The graph is now indexed by addresses of the form (base, ofs) instead of a natural number

--- src/GraphTest.thy
+++ src/GraphTest.thy
@@ -4,83 +4,50 @@ begin
 
 text \<open>This will be the content for the nodes, V is for values, U is for
   uninitialized and P is for pointer to another node.\<close>
-datatype node_content = Val int_val | U | P nat | InvalidP
+datatype node_content = Val int_val | U | P addr | InvalidP
 
 record node =
-  id :: nat
+  id :: addr
   content :: node_content
 
 definition g :: "node list_graph" where
   "g \<equiv> \<lparr> nodesL = [], edgesL = [] \<rparr>"
 
-type_synonym id_mem = "(nat \<times> val option) list option list"
 type_synonym block_mem = "val option list"
-type_synonym id_block_mem = "(nat \<times> val option) list"
-
-(* n is the last id number used the nodes *)
-fun add_id_to_block :: "nat \<Rightarrow> block_mem \<Rightarrow> (nat \<times> id_block_mem)" where
-  "add_id_to_block n [] = (n, [])"
-| "add_id_to_block n (x#xs) = 
-    (let (n', m) = add_id_to_block (n+1) xs
-    in (n', (n+1, x) # m))"
-
-fun add_id_to_mem :: "nat \<Rightarrow> mem \<Rightarrow> id_mem" where
-  "add_id_to_mem n [] = []"
-| "add_id_to_mem n (x#xs) = (case x of
-    None \<Rightarrow> None # add_id_to_mem n xs |
-    Some block \<Rightarrow>
-      let (n', bl) = add_id_to_block n block
-      in Some bl # add_id_to_mem n' xs
-  )"
-
-fun enumerate_mem :: "mem \<Rightarrow> id_mem" where
-  "enumerate_mem m = add_id_to_mem 0 m"
-
-value "enumerate_mem [Some [Some (I 1), Some (I 2), Some (I 3)], None, Some [Some (I 4), Some (I 5), Some (A (0,1))]]"
 
 abbreviation "mem \<equiv> [Some [Some (I 1), Some (I 2), Some (I 3)], None, Some [Some (I 4), Some (I 5), Some (A (0,1))]]"
 
-value "create_nodes (enumerate_mem mem) (enumerate_mem mem)"
 
-fun valid_id_mem :: "addr \<Rightarrow> id_mem \<Rightarrow> bool" where
-  "valid_id_mem (i,j) \<mu> = (
-    if i<length \<mu> then (
-      case \<mu>!i of
-        None \<Rightarrow> False
-      | Some b \<Rightarrow> 0\<le>j \<and> nat j < length b)
-    else False)"
-
-fun create_node :: "id_mem \<Rightarrow> nat \<times> val option \<Rightarrow> node" where
-  "create_node m (n, None) = \<lparr>id = n, content = U\<rparr>"
-| "create_node m (n, (Some v)) = (case v of
-      NullVal \<Rightarrow> \<lparr>id = n, content = InvalidP\<rparr> |
-      I iv \<Rightarrow> \<lparr>id = n, content = (Val iv)\<rparr> |
-      A (i,j) \<Rightarrow> (case (valid_id_mem (i,j) m) of
-        True \<Rightarrow> let id = fst (the (m!i) !! j)
-          in \<lparr>id = n, content = (P id)\<rparr> |
-        False \<Rightarrow> \<lparr>id = n, content = InvalidP\<rparr>)
+fun create_node :: "mem \<Rightarrow> addr \<Rightarrow> val option \<Rightarrow> node" where
+  "create_node m addr None = \<lparr>id = addr, content = U\<rparr>"
+| "create_node m addr (Some v) = (case v of
+      NullVal \<Rightarrow> \<lparr>id = addr, content = InvalidP\<rparr> |
+      I iv \<Rightarrow> \<lparr>id = addr, content = (Val iv)\<rparr> |
+      A (i,j) \<Rightarrow> (case (valid_mem (i,j) m) of
+        True \<Rightarrow> \<lparr>id = addr, content = (P (i,j))\<rparr> |
+        False \<Rightarrow> \<lparr>id = addr, content = InvalidP\<rparr>)
     )"
 
-fun create_block_edges :: "id_mem \<Rightarrow> id_block_mem \<Rightarrow> node list_graph" where
-  "create_block_edges m [] = \<lparr> nodesL = [], edgesL = [] \<rparr>"
-| "create_block_edges m (_#[]) = \<lparr> nodesL = [], edgesL = [] \<rparr>"
-| "create_block_edges m (x#y#xs) =
+fun create_block_edges :: "mem \<Rightarrow> addr \<Rightarrow> block_mem \<Rightarrow> node list_graph" where
+  "create_block_edges m (i,j) [] = \<lparr> nodesL = [], edgesL = [] \<rparr>"
+| "create_block_edges m (i,j) (_#[]) = \<lparr> nodesL = [], edgesL = [] \<rparr>"
+| "create_block_edges m (i,j) (x#y#xs) =
       (let
-        node1 = create_node m x;
-        node2 = create_node m y
+        node1 = create_node m (i, j) x;
+        node2 = create_node m (i, j+1) y
       in 
-        add_edge node1 node2 (create_block_edges m (y#xs)))"
+        add_edge node1 node2 (create_block_edges m (i, j+1) (y#xs)))"
 
-fun create_edges :: "id_mem \<Rightarrow> id_mem \<Rightarrow> node list_graph" where
-  "create_edges m [] = \<lparr> nodesL = [], edgesL = [] \<rparr>"
-| "create_edges m (x#xs) = (case x of
-    None \<Rightarrow> create_edges m xs |
+fun create_edges :: "mem \<Rightarrow> nat \<Rightarrow> mem \<Rightarrow> node list_graph" where
+  "create_edges m n [] = \<lparr> nodesL = [], edgesL = [] \<rparr>"
+| "create_edges m n (x#xs) = (case x of
+    None \<Rightarrow> create_edges m (n+1) xs |
     Some ys \<Rightarrow>
       (let
-        g = create_block_edges m ys;
+        g = create_block_edges m (n,0) ys;
         ng = (nodesL g);
         eg = (edgesL g);
-        g' = create_edges m xs;
+        g' = create_edges m (n+1) xs;
         ng' = (nodesL g');
         eg' = (edgesL g')
       in
@@ -88,11 +55,11 @@ fun create_edges :: "id_mem \<Rightarrow> id_mem \<Rightarrow> node list_graph" 
       )
   )"
 
-value "create_edges (enumerate_mem mem) (enumerate_mem mem)"
+value "create_edges mem 0 mem"
 
-fun lookup_node :: "nat \<Rightarrow> node list \<Rightarrow> node option" where
+fun lookup_node :: "addr \<Rightarrow> node list \<Rightarrow> node option" where
   "lookup_node _ [] = None"
-| "lookup_node n (x#xs) = (if (id x) = n then Some x else lookup_node n xs)"
+| "lookup_node addr (x#xs) = (if (id x) = addr then Some x else lookup_node addr xs)"
 
 fun create_pointer_edges :: "node list \<Rightarrow> node list_graph \<Rightarrow> node list_graph" where
   "create_pointer_edges [] ng = ng"
@@ -103,15 +70,14 @@ fun create_pointer_edges :: "node list \<Rightarrow> node list_graph \<Rightarro
     _ \<Rightarrow> create_pointer_edges xs ng
   )"
 
-abbreviation "testgraph \<equiv> create_edges (enumerate_mem mem) (enumerate_mem mem)"
+abbreviation "testgraph \<equiv> create_edges mem 0 mem"
 
 value "create_pointer_edges (nodesL testgraph) testgraph"
 
 fun construct_graph :: "mem \<Rightarrow> node list_graph" where
   "construct_graph xs = 
     (let
-      id_mem = enumerate_mem xs;
-      graph = (create_edges id_mem id_mem)
+      graph = (create_edges mem 0 mem)
     in
       create_pointer_edges (nodesL graph) graph
     )"
@@ -127,12 +93,12 @@ fun shows_node_content :: "node_content \<Rightarrow> shows" where
 | "shows_node_content (InvalidP) = shows ''<invalid>''"
 
 fun shows_node :: "node \<Rightarrow> shows" where
-  "shows_node x = shows_paren (shows (id x) o shows '', '' o shows_node_content (content x))"
+  "shows_node x = shows_paren (shows (A (id x)) o shows '', '' o shows_node_content (content x))"
 
 fun shows_edge :: "node \<times> node \<Rightarrow> shows" where
   "shows_edge (x,y) = shows_paren (shows_sep shows_node (shows '', '') [x,y])"
 
-value "shows_node \<lparr>node.id = 1, content = Val 43\<rparr> ''''"
+value "shows_node \<lparr>node.id = (1, 0), content = Val 43\<rparr> ''''"
 
 fun shows_graph :: "node list_graph \<Rightarrow> shows" where
   "shows_graph graph =
