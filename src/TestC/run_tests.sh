f0335909fc08a097b801c4836dbd219e8fc57491
Gabriela Limonta <glimonta@gmail.com>
2015-06-23 17:07:35 +0200

    New Makefile and test-running script
    Calling run_tests will run all the test files and will print to stdout
    in case of failed tests.

---
+++ src/TestC/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./bubblesort_test
+./count_test
+./fact_test
+./fib_test
+./mergesort_test
+./min_test
+./occurs_test
+./quicksort_test
+./selection_test
+./strlen_test
+./plus_test
+./subst_test
