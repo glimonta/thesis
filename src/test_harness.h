5f50712659d0d0013caa70654b47a67156cb9b4c
Gabriela Limonta <glimonta@gmail.com>
2015-06-23 15:16:28 +0200

    Deleted files that are generated automatically.

---
+++ src/test_harness.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <inttypes.h>
+#include "hashset.h"
+
+hashset_t discovered;
+int num_tests = 0;
+int passed = 0;
+int failed = 0;
+
+#define __TEST_HARNESS_DISCOVER(addr, var) hashset_add(discovered, addr); var = addr;
+
+#define __TEST_HARNESS_ASSERT_EQ(var, val) ++num_tests; (var != val) ? ++failed : ++passed;
+
+#define __TEST_HARNESS_ASSERT_EQ_NULL(var) ++num_tests; (&var != NULL) ? ++failed : ++passed;
+
+#define __TEST_HARNESS_ASSERT_EQ_PTR(var, val) ++num_tests; (var != val) ? ++failed : ++passed;
